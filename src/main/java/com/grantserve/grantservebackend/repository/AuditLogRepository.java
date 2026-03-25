46178a3ff5db74b2d8ca1de3c36d973b089564da
Rushitha <2478345@cognizant.com>
2026-03-25 16:23:04 +0530

    project structure formation-reporting analytics

--- src/main/java/com/grantserve/grantservebackend/repository/AuditLogRepository.java
+++
@@ -1,6 +0,0 @@
-package com.grantserve.grantservebackend.repository;
-
-import com.grantserve.grantservebackend.entity.AuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {}
